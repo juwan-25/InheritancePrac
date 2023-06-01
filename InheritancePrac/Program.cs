4180f172dc6da04f2ecf97e8f777b82964dd3d44
Juwan Lee <ljw05w5@gmail.com>
2023-06-01 09:46:38 +0900

    다형성을 활용하기 위한 is와 as 키워드

--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -22,6 +22,22 @@ namespace InheritancePrac
             {
                 item.Eat();
                 item.Sleep();
+
+                var _dog = item as Dog;
+                if (_dog != null) { _dog.Bark(); }
+
+                var _cat = item as Cat;
+                if(_cat != null ) { _cat.Meow(); }
+
+                //클래스에 따라 다른 메서드 호출
+                if(item is Dog)
+                {
+                    ((Dog)item).Bark();
+                }
+                else if(item is Cat) 
+                {
+                    ((Cat)item).Meow();
+                }
             }
         }
     }
