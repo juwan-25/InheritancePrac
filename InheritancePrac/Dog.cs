b84950d1215c445aa74f58ccb93e352f7497cb20
Juwan Lee <ljw05w5@gmail.com>
2023-06-01 09:09:36 +0900

    상속을 사용하지 않고 클래스 설계

---
+++ InheritancePrac/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InheritancePrac
+{
+    public class Dog
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Dog()
+        {
+            this.Age = 0;
+        }
+        public void Eat()
+        {
+            Console.WriteLine("냠냠 먹습니다");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("왈왈 짓습니다");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨 잡니다");
+        }
+    }
+}
--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace InheritancePrac
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
         }
     }
 }
